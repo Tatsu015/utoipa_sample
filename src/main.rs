158dbf77eb260c87fcce1e67e64bdf13516f1262
Tatsu015 <tatsumi.soichiro@gmail.com>
2025-08-20 19:59:57 +0900

    add get user route

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-use axum::{Router, routing::get};
+use axum::{Json, Router, routing::get};
+use serde::Serialize;
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    let app = Router::new().route("/", get(root));
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/user", get(get_user));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
@@ -12,4 +15,18 @@ async fn main() {
 
 async fn root() -> &'static str {
     "Hello"
+}
+
+#[derive(Serialize)]
+struct User {
+    id: u8,
+    name: &'static str,
+}
+
+async fn get_user() -> Json<User> {
+    let user = User {
+        id: 1,
+        name: "sample_name",
+    };
+    Json(user)
 }
