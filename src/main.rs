7b3d4738a4466469e2c2db9d7acb24157fcfcb11
Tatsu015 <tatsumi.soichiro@gmail.com>
2025-08-20 22:16:17 +0900

    success to add axum

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,33 @@
 use axum::{Json, Router, routing::get};
 use serde::Serialize;
+use utoipa::openapi::OpenApi;
+use utoipa_axum::router::OpenApiRouter;
+use utoipa_axum::routes;
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::fmt::init();
+    let (router, api) = OpenApiRouter::new().routes(routes!(root)).split_for_parts();
 
-    let app = Router::new()
-        .route("/", get(root))
-        .route("/user", get(get_user));
-
+    let app = router.into_make_service();
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
+#[utoipa::path(get, path = "/")]
 async fn root() -> &'static str {
     "Hello"
 }
 
-#[derive(Serialize)]
-struct User {
-    id: u8,
-    name: &'static str,
-}
+// #[derive(Serialize)]
+// struct User {
+//     id: u8,
+//     name: &'static str,
+// }
 
-async fn get_user() -> Json<User> {
-    let user = User {
-        id: 1,
-        name: "sample_name",
-    };
-    Json(user)
-}
+// async fn get_user() -> Json<User> {
+//     let user = User {
+//         id: 1,
+//         name: "sample_name",
+//     };
+//     Json(user)
+// }
